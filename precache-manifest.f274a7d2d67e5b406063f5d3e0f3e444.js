92ed10ba845a5b38eff23de0c3ce14b395de23fe
imsachi <63872913+imsachi@users.noreply.github.com>
2020-08-13 21:32:12 +0530

    Updates

---
+++ precache-manifest.f274a7d2d67e5b406063f5d3e0f3e444.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a6f4a539cfad35144fb6908edd2e77",
+    "url": "/test2/index.html"
+  },
+  {
+    "revision": "a1b248891d9441fa3bfb",
+    "url": "/test2/static/js/2.07374824.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test2/static/js/2.07374824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9e9e6bf4e5a15f95307",
+    "url": "/test2/static/js/main.32331f04.chunk.js"
+  },
+  {
+    "revision": "64b17dba3a1e6860cbac",
+    "url": "/test2/static/js/runtime-main.70875b4d.js"
+  }
+]);
